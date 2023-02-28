3fa73a4469242d58d3109e923c20e79959be3649
Timo Nohe <timo.ademeit@gmx.de>
2023-02-28 12:31:17 +0100

    29. Analyzing a Standard React Project

--- apps/react-complete-guide/src/app/app.tsx
+++ apps/react-complete-guide/src/app/app.tsx
@@ -1,15 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-
-import NxWelcome from './nx-welcome';
-
-export function App() {
+function App() {
   return (
-    <>
-      <NxWelcome title="react-complete-guide" />
-
-      <div />
-    </>
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
   );
 }
 
